03a79e592de126030ee2b137ea025a9ac7b13349
egoson <egoson@users.noreply.github.com>
2021-07-25 13:02:51 +0000

    deploy: ae086d16d402cf83fa609753980e5f80357188d6

---
+++ _nuxt/static/1627218169/thank-you/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fegoson.githab.io\u002F_nuxt\u002Fstatic\u002F1627218169",layout:"default",error:a,serverRendered:true,routePath:"\u002Fthank-you",config:{_app:{basePath:"\u002Fegoson.githab.io\u002F",assetsPath:"\u002Fegoson.githab.io\u002F_nuxt\u002F",cdnURL:a}}}}(null));
