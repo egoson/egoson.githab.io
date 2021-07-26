de38f4a35666958261340715acef0ee42d784ef8
den <myegoson@gmail.com>
2021-07-26 12:39:06 +0400

    custom domain

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,33 +1,31 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: "static",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Стилист Визажист Филипкина Дарья',
+    title: "Стилист Визажист Филипкина Дарья",
     htmlAttrs: {
-      lang: 'en'
+      lang: "ru",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
-      { description: 'Свадебные прически, свадебный макияж, обучение на визажиста, коррекция и окраска бровей и другие услуги смотрите на моей странице' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
+      {
+        description:
+          "Свадебные прически, свадебный макияж, обучение на визажиста, коррекция и окраска бровей и другие услуги смотрите на моей странице",
+      },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '~/styles/style'
-  ],
+  css: ["~/styles/style"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -35,31 +33,30 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
+    "@nuxtjs/eslint-module",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/bootstrap
-    'bootstrap-vue/nuxt',
+    "bootstrap-vue/nuxt",
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  },
+  build: {},
 
   loading: {
-    color: 'green',
-    height: '1px',
+    color: "green",
+    height: "1px",
   },
 
   pageTransition: {
-    name: 'fade',
-    mode: 'out-in',
+    name: "fade",
+    mode: "out-in",
   },
 }
