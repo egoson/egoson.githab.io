c1f0e9382a99c7157473d7fe9f58a6dbdbfe73b9
Denis <myegoson@gmail.com>
2022-01-08 13:39:43 +0400

    change price

--- pages/form.vue
+++ pages/form.vue
@@ -164,7 +164,7 @@ export default {
             },
             {
               title: 'Окрашивание краской / хной',
-              cost: '400',
+              cost: '450',
               brow: true
             },
             {
